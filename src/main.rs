4a8c473ed25ff74ce35b5194edcdea2cae99106c
Maxime Ripard <maxime.ripard@anandra.org>
2018-10-21 18:22:35 +0200

    parser: Fix timestamp format
    Signed-off-by: Maxime Ripard <maxime.ripard@anandra.org>

--- src/main.rs
+++ src/main.rs
@@ -27,28 +27,28 @@ impl fmt::Debug for RegisterWrite {
 named!(timestamp_parser,
        delimited!(
            tag!("[ "),
-           take_until!(" ]"),
-           tag!(" ]")
+           take_until!("]"),
+           tag!("]")
        )
 );
 
 #[test]
 fn timestamp_parser_test() {
-    let string = "[ 1.002540 ] 1ca0000.dsi: reg=0x42424242 val=0x84848484";
+    let string = "[ 1.002540] 1ca0000.dsi: reg=0x42424242 val=0x84848484";
     let timestamp = "1.002540";
     let rest = " 1ca0000.dsi: reg=0x42424242 val=0x84848484";
 
     assert_eq!(timestamp_parser(string.as_bytes()),
                Ok((rest.as_bytes(), timestamp.as_bytes())));
 
-    let string = "[ 1.002540 ]";
+    let string = "[ 1.002540]";
     let rest = "";
     assert_eq!(timestamp_parser(string.as_bytes()),
                Ok((rest.as_bytes(), timestamp.as_bytes())));
 
-    let string = "[ 1.002540 ";
+    let string = "[ 1.002540";
     assert_eq!(timestamp_parser(string.as_bytes()),
-               Err(nom::Err::Incomplete(nom::Needed::Size(2))));
+               Err(nom::Err::Incomplete(nom::Needed::Size(1))));
 
     let string = "[";
     assert_eq!(timestamp_parser(string.as_bytes()),
